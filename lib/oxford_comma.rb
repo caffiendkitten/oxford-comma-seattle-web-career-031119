edfe5ca1b89a19006b46572014872796beaf9202
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-03-03 21:14:57 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,17 @@ def oxford_comma(array)
   elsif array.size == 2
     return array.join(" and ")
   elsif array.size == 3
-    temp array = []
-    temp_array.push(array.join(", "))
-    return temp_array.insert(-2, "and ")
+    temp_array = []
+
+    temp_array = array.insert(-2,"and")
+
+    temp_array = temp_array.join(", ")
+    return temp_array
   else
     return array
   end
 end
+
+array = ["saam", "John", "Sarah"]
+
+oxford_comma(array)
