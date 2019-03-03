a221c8dd9e99f838e6fc19f18c34c5ae49db80ff
DaNeil Coulthard <caffiendkitten@gmail.com>
2019-03-03 20:46:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.size == 1
     return array.join
   elsif array.size == 2
-    return array.insert(array[-1], " and ")
+    return array.insert(-1, " and ")
   elsif array.size == 3
     return array.join(", and ")
   else
